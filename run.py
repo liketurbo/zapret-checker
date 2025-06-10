5172d0d43d8fed696618c8e382bcd19c81f6a68d
Rami Chasygov <liketurbo.work@gmail.com>
2025-06-10 20:24:17 +0300

    add restart zapret

--- run.py
+++ run.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
+import subprocess
 from config_utils import get_configs, update_config
 from network_checks import check_yandex_access, check_youtube_access, check_instagram_access
 from log import setup_logging
+
+
+def restart_zapret():
+    try:
+        subprocess.run(
+            ["/etc/init.d/zapret", "restart"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        return True
+    except:
+        return False
 
 
 def main():
@@ -23,6 +37,11 @@ def main():
         config = all_configs[config_name]
         logger.info(f"checking config {config_name}")
         update_config(config)
+
+        success = restart_zapret()
+        if not success:
+            logger.error("failed to restart zapret")
+            return
 
         youtube_access = check_youtube_access()
         if not youtube_access:
@@ -45,9 +64,11 @@ def main():
     if not instagram_config:
         logger.warning("found config that only works for youtube")
         update_config(youtube_config)
+        restart_zapret()
     if not youtube_config:
         logger.warning("found config that only works for instagram")
         update_config(instagram_config)
+        restart_zapret()
 
 
 if __name__ == "__main__":
